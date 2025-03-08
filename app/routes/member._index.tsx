0182e61b0f484e0cb19c4c3d5c02d84ef3a20aa4
Arie Ardiansyah <kelivdc@yahoo.com>
2025-03-08 16:15:29 +0700

    Remove log

--- app/routes/member._index.tsx
+++ app/routes/member._index.tsx
@@ -1,5 +1,6 @@
 import { LoaderFunctionArgs, json } from "@remix-run/node";
 import { useLoaderData, useNavigate } from "@remix-run/react";
+import React, { useState, useEffect } from 'react';
 import { Button } from "~/components/ui/button";
 import { getSession } from "~/sessions";
 
@@ -13,7 +14,7 @@ export async function loader({ request }: LoaderFunctionArgs) {
     request.headers.get("Cookie")
   );
   const jwt = session.get("jwt")
-  let url = `${process.env.SERVER}/soals`
+  let url = `${process.env.SERVER}/soals?populate=Topik&filters[Topik][mulai][$lte]=1004-01-26`
   const response = await fetch(url, {
     method: 'GET',
     headers: {
@@ -22,20 +23,28 @@ export async function loader({ request }: LoaderFunctionArgs) {
     }
   })
   const result = await response.json()
-  const total = await result.meta.pagination.total;
+  const total = await result.meta.pagination.total;  
+
   return json({ data: session, total: total })
 }
 
+
 export default function Member() {
-  const navigate = useNavigate()
-  const loader = useLoaderData()
+  const navigate = useNavigate()  
+  const loader = useLoaderData()  
+  // const url = loader?.server
+  // useEffect(() => {
+  //   console.log(loader?.server)
+  // },[loader?.server])   
   return (
     <div className="h-screen grid place-items-center text-center">
-      <div>
+      <div>        
         {loader.total > 0 ? (
+          <>
           <div>Untuk memulai ujian. Silahkan klik tombol MULAI<br />
             <Button className="mt-4" onClick={() => navigate('/member/soal')}>MULAI</Button>
           </div>
+          </>
         ) : (
           <div>Maaf, untuk saat ini Anda tidak ada soal yang tersedia. Silahkan hubungi pengawas<br />
             <Button className="mt-4" onClick={() => navigate('/logout')}>TUTUP</Button>
